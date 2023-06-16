5bbb400f315f8c974ae181d5c9ee5c37fc89050c
narendrasingh26 <narendrajadon261990@gmail.com>
2023-06-16 20:21:52 +0530

    10

--- src/component/Mainpage.js
+++ src/component/Mainpage.js
@@ -1,11 +1,34 @@
-import React, { useState } from "react";
-import { Form, Button, ListGroup, Row, Col} from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Form, Button, ListGroup, Row, Col } from "react-bootstrap";
+import axios from "axios";
 
 const AddExpense = () => {
   const [moneySpent, setMoneySpent] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
   const [products, setProducts] = useState([]);
+  const [expenseId, setExpenseId] = useState("");
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
+  const fetchExpenses = () => {
+    axios
+      .get("https://expense-tracker-c5ab0-default-rtdb.firebaseio.com/expenses.json")
+      .then((response) => {
+        if (response.data) {
+          const expenses = Object.entries(response.data).map(([id, expense]) => ({
+            id,
+            ...expense,
+          }));
+          setProducts(expenses);
+        }
+      })
+      .catch((error) => {
+        console.log("Error fetching expenses:", error);
+      });
+  };
 
   const handleMoneySpentChange = (event) => {
     setMoneySpent(event.target.value);
@@ -23,7 +46,6 @@ const AddExpense = () => {
     event.preventDefault();
 
     const newProduct = {
-      id: Math.random().toString(),
       moneySpent,
       description,
       category,
@@ -32,22 +54,78 @@ const AddExpense = () => {
     setMoneySpent("");
     setDescription("");
     setCategory("");
+    setExpenseId("");
 
-    setProducts((prevProducts) => [...prevProducts, newProduct]);
+    if (expenseId) {
+      // Update existing expense
+      axios
+        .put(
+          `https://expense-tracker-c5ab0-default-rtdb.firebaseio.com/expenses/${expenseId}.json`,
+          newProduct
+        )
+        .then((response) => {
+          console.log("Expense successfully updated!");
+          fetchExpenses();
+        })
+        .catch((error) => {
+          console.log("Error updating expense:", error);
+        });
+    } else {
+      // Add new expense
+      axios
+        .post(
+          "https://expense-tracker-c5ab0-default-rtdb.firebaseio.com/expenses.json",
+          newProduct
+        )
+        .then((response) => {
+          console.log("Data saved successfully!");
+          fetchExpenses();
+        })
+        .catch((error) => {
+          console.log("Error saving data:", error);
+        });
+    }
+  };
+
+  const handleExpenseDelete = (id) => {
+    axios
+      .delete(
+        `https://expense-tracker-c5ab0-default-rtdb.firebaseio.com/expenses/${id}.json`
+      )
+      .then((response) => {
+        console.log("Expense successfully deleted!");
+        fetchExpenses();
+        setProducts((prevProducts) => prevProducts.filter((product) => product.id !== id));
+      })
+      .catch((error) => {
+        console.log("Error deleting expense:", error);
+      });
+  };
+
+  const handleExpenseEdit = (id) => {
+    const editedExpense = products.find((product) => product.id === id);
+    if (editedExpense) {
+      setMoneySpent(editedExpense.moneySpent);
+      setDescription(editedExpense.description);
+      setCategory(editedExpense.category);
+      setExpenseId(id);
+    }
   };
 
   return (
-    // <Container fluid className="w-100">
-
-    
-    <div   style={{
-      backgroundImage: "url(cart.avif)",
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      minHeight: "100vh",
-      padding:'1rem'
-    }}>
-      <h1 style={{textAlign:'center',fontFamily:'serif',fontWeight:'bold'}}> Daily Expense</h1><br></br>
+    <div
+      style={{
+        backgroundImage: "url(cart.avif)",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+        padding: "1rem",
+      }}
+    >
+      <h1 style={{ textAlign: "center", fontFamily: "serif", fontWeight: "bold" }}>
+        Daily Expense
+      </h1>
+      <br />
       <Form onSubmit={handleExpenseSubmit}>
         <Row>
           <Col>
@@ -85,19 +163,35 @@ const AddExpense = () => {
               </Form.Control>
             </Form.Group>
           </Col>
-        </Row><br></br>
-        <Button type="submit" style={{marginLeft:'30rem'}}>Add Expense</Button>
+        </Row>
+        <br />
+        <Button type="submit" style={{ marginLeft: "30rem" }}>
+          {expenseId ? "Update Expense" : "Add Expense"}
+        </Button>
       </Form>
 
       <div>
         <h3>Products:</h3>
         {products.length > 0 ? (
-          <ListGroup >
+          <ListGroup>
             {products.map((product) => (
-              <ListGroup.Item key={product.id} style={{backgroundColor:'#98ecd2'}}>
-                <p>Money Spent: {product.moneySpent}</p>
-                <p>Description: {product.description}</p>
-                <p>Category: {product.category}</p>
+              <ListGroup.Item
+                key={product.id}
+                style={{ backgroundColor: "#98ecd2", display: "flex", justifyContent: "space-between" }}
+              >
+                <div>
+                  <p>Money Spent: {product.moneySpent}</p>
+                  <p>Description: {product.description}</p>
+                  <p>Category: {product.category}</p>
+                </div>
+                <div>
+                  <Button variant="danger" onClick={() => handleExpenseDelete(product.id)}>
+                    Delete
+                  </Button>{" "}
+                  <Button variant="info" onClick={() => handleExpenseEdit(product.id)}>
+                    Edit
+                  </Button>{" "}
+                </div>
               </ListGroup.Item>
             ))}
           </ListGroup>
@@ -106,7 +200,6 @@ const AddExpense = () => {
         )}
       </div>
     </div>
-    // </Container>
   );
 };
 
